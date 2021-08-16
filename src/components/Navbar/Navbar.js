d470bba1f308ad244e119257604bbed7c8dae56b
Piotr Zajas <zajas.piotr@gmail.com>
2021-08-17 00:08:25 +0200

    Add a navbar to the application

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,23 @@
+import "./Navbar.css";
+
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <nav className="navbar">
+      <div className="brand-title">BRAND NAME</div>
+      <div className="navbar-links">
+        <ul>
+          <li className="li">
+            <NavLink to="/main">MAIN</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">ABOUT</NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
