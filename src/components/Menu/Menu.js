fd77eb7ed11437ace21fce6fa48141ff53772d58
Piotr Zajas <zajas.piotr@gmail.com>
2021-08-20 01:01:19 +0200

    Add a carlike look for the menu items

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,13 +1,32 @@
 import { useState } from "react";
 
+import Burger1 from "../../assets/Menu/Transparent/Burger1.png";
+
 import "./Menu.css";
 
 const Menu = () => {
   const initialState = [
-    { id: 1, name: "Hamburger" },
-    { id: 2, name: "Pizza" },
-    { id: 3, name: "Hot-Dog" },
-    { id: 4, name: "Carbonara" },
+    {
+      id: 1,
+      name: "Hamburger",
+      description:
+        "A hamburger (also burger for short) is a food, typically considered a sandwich",
+      price: "$10",
+    },
+    {
+      id: 2,
+      name: "Hamburger 2",
+      description:
+        "A hamburger (also burger for short) is a food, typically considered a sandwich",
+      price: "$15",
+    },
+    {
+      id: 1,
+      name: "Hamburger for Poor",
+      description:
+        "A hamburger (also burger for short) is a food, typically considered a sandwich",
+      price: "$8",
+    },
   ];
 
   const [menu, setMenu] = useState(initialState);
@@ -16,8 +35,15 @@ const Menu = () => {
     <div className="menu">
       <ul>
         {menu.map((item) => (
-          <li key={item.id} className="menu__item">
-            {item.name}
+          <li key={item.id} className="menu-item">
+            <div className="item-photo">
+              <img src={Burger1} alt="Menu item" />
+            </div>
+            <div className="item-name">{item.name}</div>
+            <div className="item-description"> {item.description}</div>
+            <div className="item-price"> {item.price}</div>
+            <button>BUY</button>
+            {/* <div className="menu-description"></div> */}
           </li>
         ))}
       </ul>
